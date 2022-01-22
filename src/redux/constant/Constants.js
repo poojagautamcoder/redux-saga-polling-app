4d24673ba83bfda258c84d29081ba2bd1eb3d056
pooja <poojatnp4@gmail.com>
2022-01-22 16:30:44 +0530

    signup-api

--- src/redux/constant/Constants.js
+++ src/redux/constant/Constants.js
@@ -1,3 +1,7 @@
 export const Login_Request = "Login_Request";
 export const Login_Success = "Login_Success";
 export const Login_Error = "Login_Error";
+
+export const Signup_Request = "Signup_Request";
+export const Signup_Success = "Signup_Success";
+export const Signup_Error = "Signup_Error";
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import LoginReducer from "./LoginReducer";
+import SignupReducer from "../reducers/SignupReducer";
 const rootReducer = combineReducers({
   LoginReducer,
+  SignupReducer,
 });
 export default rootReducer;
