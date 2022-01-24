6f9df5cd829899471418c3481b1d093e46fec3c8
arunetech <arun.etech11@gmail.com>
2022-01-24 12:27:24 +0530

    poll-ui

--- src/screens/dashboard/DashBoard.js
+++ src/screens/dashboard/DashBoard.js
@@ -1,5 +1,30 @@
 import React from "react";
+const DashBoard = () => {
+  return (
+    <div className="poll-details">
+      <div className="table-responsive polldata">
+        <h3>Title</h3>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+          />
+          <h5>options</h5>
+          <div className="pollsbtn">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
 
-export default function DashBoard() {
-  return <div>DashBoard</div>;
-}
+            <button type="button" className="btn btn-info edit">
+              Edit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoard;
