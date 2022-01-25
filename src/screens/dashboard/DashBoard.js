cdc552a9de7bfe89948be1e94d1c7943e78f68ee
pooja <poojatnp4@gmail.com>
2022-01-25 17:21:51 +0530

    logout-button

--- src/screens/dashboard/DashBoard.js
+++ src/screens/dashboard/DashBoard.js
@@ -3,6 +3,7 @@ import DashPoll from "../../components/DashPoll";
 import PollOption from "../../components/PollOption";
 import { useDispatch, useSelector } from "react-redux";
 import { DashboardRequest } from "../../redux/action/index";
+import { Link } from "react-router-dom";
 const DashBoard = () => {
   const { data } = useSelector((state) => state.DashboardReducer);
   const dispatch = useDispatch();
@@ -12,24 +13,33 @@ const DashBoard = () => {
   }, []);
 
   return (
-    <div className="poll-details ">
-      {data &&
-        data.length &&
-        data?.map((curElem, index) => {
-          return (
-            <div key={curElem._id}>
-              <DashPoll data={curElem} />
-              <div className="pollsbtn">
-                <button type="button" className="btn btn-danger">
-                  Delete
-                </button>
-                <button type="button" className="btn btn-info edit">
-                  Edit
-                </button>
+    <div className="poll-details Screen-page">
+      <div className="dashboard">
+        {data &&
+          data.length &&
+          data?.map((curElem, index) => {
+            return (
+              <div key={curElem._id}>
+                <DashPoll data={curElem} />
+                <div className="pollsbtn">
+                  <button type="button" className="btn btn-danger">
+                    Delete
+                  </button>
+                  <button type="button" className="btn btn-info edit">
+                    Edit
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
+      <div className="logout-button">
+        <Link className="nav-link" to="/">
+          <button type="button" class="btn btn-success">
+            Logout
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
